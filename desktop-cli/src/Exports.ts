dda49f1f51079849a1d1d46f2e1ead609cb625ab
XFaonAE <xfaonae@protonmail.com>
2021-12-14 15:20:09 -0500

    Installed term in cli desk

--- desktop-cli/src/Exports.ts
+++ desktop-cli/src/Exports.ts
@@ -1,2 +1,23 @@
-const message = "Hello, world";
-export { message };
+import { anime, CommandParser, logging } from "@illuxdev/oxide-terminal";
+import DevFlags from "./DevFlags";
+
+const cli = new CommandParser();
+
+cli.registerCommand<DevFlags>({
+    name: "dev",
+    description: "Start a development server for the app",
+    handler: (args, flags) => {
+        if (flags.readConfig == undefined || flags.readConfig == true) {
+            
+        }
+    },
+    options: [
+        {
+            name: "readConfig",
+            type: "boolean",
+            description: "Should OxideDesktopCli read the application config"
+        }
+    ]
+})
+
+cli.execute();
